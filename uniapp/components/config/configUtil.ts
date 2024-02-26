1f9f7009a35f739c7457775760341657954cdb02
LuBaobei <hobairiku@gmail.com>
2024-02-26 18:04:33 +0800

    feat：调整全局Config类的定义方式

--- uniapp/components/config/configUtil.ts
+++ uniapp/components/config/configUtil.ts
@@ -1,39 +1,45 @@
 // 这个模块全局嵌入一个CONFIG对象，用于uniapp配置全局参数
-import { Platform, platformIsBrowser } from './platform'
+import {Platform, platformIsBrowser} from './platform'
 
 // 全局配置对象，每一个模式对应一个类型
-export type Config<ExtendType = Record<string, any>> = {
-  // 主后端服务地址
-  backendUrl: string
-  // 主远程静态文件服务地址
-  remoteStaticUrl: string
-  // 是否是浏览器环境
-  isBrowser: boolean
-  // 当前平台类型
-  platform: string
-  // 当前是否是开发模式
-  isDev: boolean
-} & ExtendType
+declare global {
+  interface GlobalConfig {
+    // 主后端服务地址
+    backendUrl: string
+    // 主远程静态文件服务地址
+    remoteStaticUrl: string
+    // 是否是浏览器环境
+    isBrowser: boolean
+    // 当前平台类型
+    platform: string
+    // 当前是否是开发模式
+    isDev: boolean
+  }
+}
 
-const emptyConfig: Config = {
+const emptyConfig: GlobalConfig = {
   backendUrl: '',
   remoteStaticUrl: '',
   isBrowser: false,
   platform: '',
   isDev: false,
 }
+
 export class ConfigUtil {
   // 所有模式的配置
-  private ConfigList: Record<string, Config>
+  private ConfigList: Record<string, GlobalConfig>
+
   constructor() {
     this.ConfigList = {}
   }
+
   // 注册配置，提供一个modeKey， 一个配置对象
-  public registerConfig(modeKey: string, config: Partial<Config>) {
-    this.ConfigList[modeKey] = Object.assign({},emptyConfig, config)
+  public registerConfig(modeKey: string, config: Partial<GlobalConfig>) {
+    this.ConfigList[modeKey] = Object.assign({}, emptyConfig, config)
   }
+
   // 获取配置，并安装应用到全局, 这里需要使用者自行定义扩展，因为xtendType需要指定
-  public installConfig(modeKey: string, overrideConfig?: Partial<Config>) {
+  public installConfig(modeKey: string, overrideConfig?: Partial<GlobalConfig>) {
     const config = this.ConfigList[modeKey]
     // 根据当前platform，判断isBrowser和platform
     config.isBrowser = platformIsBrowser
@@ -44,8 +50,9 @@ export class ConfigUtil {
     // 合并配置
     ;(globalThis as any).CONFIG = Object.assign({}, config, overrideConfig || {})
   }
+
   // 指定配置key列表，用于生成local文件，内容就是按照es6语法export default {} 生成
-  static makeConfigTemplateString(config: Partial<Config>) {
+  static makeConfigTemplateString(config: Partial<GlobalConfig>) {
     // 这里不使用JSON.stringify，因为JSON.stringify会把key加上双引号，这里按照一行一行保持两个空格缩紧输出
     let str = `export default {\n`
     for (const key in config) {
